575d69cf32bbbc26d752678382274e2205dfb0ab
bjorgvinkodehode <125961567+bjorgvinkodehode@users.noreply.github.com>
2024-09-17 07:37:55 +0000

    changed url in cv

--- src/About.jsx
+++ src/About.jsx
@@ -37,7 +37,7 @@ function About() {
             </div>
                     
                     
-                    
+                     
                     
         </section>
           
